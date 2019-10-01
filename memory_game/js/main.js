3d90949cd0943c196715f07984401247d5db1bd3
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2019-10-01 08:56:13 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen"
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen" , "queen" , "king" , "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+    if (cardOne === cardTwo) {
+        alert("You found a match!")
+    } else { 
+    alert("Sorry, try again")
+    }
+}
