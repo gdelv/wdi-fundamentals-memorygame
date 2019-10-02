a1f067b9083f852a1281a96781571ac63355d89f
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2019-10-02 16:46:06 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 console.log("Up and running!");
-var cards = ["queen" , "queen" , "king" , "king"];
+var cards = [
+        {
+            rank: 'queen',
+            suit: 'hearts',
+            cardImage: 'images/queen-of-hearts.png'
+        },
+        {
+            rank: 'queen',
+            suit: 'diamonds',
+            cardImage: 'images/queen-of-diamonds.png'   
+        },
+        {
+            rank: 'king',
+            suit: 'hearts',
+            cardImage: 'images/king-of-hearts.png'
+        },
+        {
+            rank: 'king',
+            suit: 'diamonds',
+            cardImage: 'images/king-of-diamonds.png'
+        }
+];
 var cardsInPlay = [];
 
 function checkForMatch () {
@@ -12,9 +33,10 @@ function checkForMatch () {
 
 //cardId is a number (0-3)
 function flipCard (cardId) {
-    
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log(cards[cardId].cardImage);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log("The suit is "+ cards[cardId].suit);
+    cardsInPlay.push(cards[cardId].rank);
     return checkForMatch();
 
 
